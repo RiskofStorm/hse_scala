2720ec60e0c48b496b00bdee5bfbb99275e8faf5
Daniel Abramov <riskofstorm@gmail.com>
2026-01-31 02:07:11 +0300

    done hw1 long time ago, hw2 todo

---
+++ src/main/scala/hw_1/MergeSort.scala
@@ -0,0 +1,52 @@
+package hw_1
+
+
+//@tailrec
+def mergeSort(vector: Vector[Int]) : Vector[Int] = {
+    if (vector.length <= 1) return vector
+
+    val mid = vector.length / 2
+    val (leftPart, rightPart) = vector.splitAt(mid)
+  
+    val left = mergeSort(leftPart)
+    val right = mergeSort(rightPart)
+
+    merge(left, right)
+}
+
+def merge(left:Vector[Int], right: Vector[Int]): Vector[Int] = {
+  val merged: ListBuffer[Int] = ListBuffer()
+  var lId = 0
+  var rId = 0
+  
+  while (lId < left.length && rId < right.length){
+      if (left(lId) < right(rId)){
+        merged.append(left(lId))
+        lId += 1
+      }else {
+        merged.append(right(rId))
+        rId += 1
+      }
+
+   }
+  while (lId < left.length) {merged.append(left(lId)) ; lId += 1}
+  while (rId < right.length) {merged.append(right(rId)); rId += 1}
+
+  merged.toVector
+
+}
+
+
+
+@main
+def main(): Unit = {
+
+  val data: Vector[Int] = Vector(9,1,4,0,5,6,2,8,3)
+  print(mergeSort(data).toString())
+
+}
+
+
+
+
+
---
+++ src/main/scala/hw_1/TopNsort.scala
@@ -0,0 +1,35 @@
+package hw_1
+
+
+// TOP N minimal values
+def topNsort(vector: Vector[Int], n: Int) : Vector[Int] = {
+  if (vector.length <= 1 || n <= 0) return vector
+  val (topNvecStart, restVec) = vector.splitAt(n)
+  val topN: ListBuffer[Int] = ListBuffer(topNvecStart*)
+
+  for (v <- restVec) {
+    val topnMax = topN.max
+    if (v < topnMax){
+      topN(topN.indexOf(topnMax)) = v
+    }
+  }
+  topN.toVector
+}
+
+// Я не сделал сортировку topN, в ТЗ этого нету, просто топ N
+@main
+def topnMain(): Unit = {
+
+//  println(topNsort(Vector(1,23,4,5,5,5,1,1), 4))
+  println(topNsort(Vector(12,140,4,5,5,5,1,1,123,0, 88,9), 5))
+  println(topNsort(Vector(9, 1, 4, 0, 5, 6, 2, 8, 3), 4)) // Vector(0,1,2,3)
+
+//  print(topNsort(Vector(5, 4, 3, 2, 1), 3)) // Vector(1,2,3)
+
+//  print(topNsort(Vector(1, 1, 1, 23, 4), 3)) // Vector(1,1,1)
+
+//  println(topNsort(Vector(-5, 3, -1, 0, -3), 3)) // Vector(-5,-3,-1)
+
+//  print(topNsort(Vector(100, 50, 20, 10, 5, 1), 2)) // Vector(1,5)
+}
+
